bafbffb214ff40864194cc82a149cabb2ea8595e
yetto-tools <ergonzalez209@gmail.com>
2020-02-26 09:07:07 -0600

    init repository invoice print project

---
+++ src/js/db.js
@@ -0,0 +1,202 @@
+var createStatement =
+  "CREATE TABLE IF NOT EXISTS Contacts(" +
+    "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+    " Nombre TEXT, Nit TEXT," +
+    " Direccion TEXT, Telefono TEXT)";
+var selectAllStatement = "SELECT * FROM Contacts";
+var insertStatement = "INSERT INTO Contacts (Nombre, Nit, Direccion, Telefono) VALUES (?, ?, ?, ?)";
+var updateStatement = "UPDATE Contacts SET Nombre = ?, Nit = ? , Direccion =?, Telefono=? WHERE id=?";
+var deleteStatement = "DELETE FROM Contacts WHERE id=?";
+var dropStatement = "DROP TABLE Contacts";
+var db = openDatabase("ContactosYClientes", "1.0", "ContactosYClientes", 50*1024*1024);  // Open SQLite Database
+var dataset;
+var DataType;
+
+function initDatabase(){  // Function Call When Page is ready.{
+
+ try {
+   if (!window.openDatabase){  // Check browser is supported SQLite or not
+     alert('Databases are not supported in this browser.');
+   }
+   else{
+     createTable();  // If supported then call Function for create table in SQLite
+   }
+ }
+catch (e) {
+  if (e == 2) {
+    // Version number mismatch.
+    console.log("Invalid database version.");
+  }
+  else {
+    console.log("Unknown error " + e + ".");
+  }
+  return;
+  }
+}
+function createTable(){  // Function for Create Table in SQLite.
+  db.transaction(function (tx) {
+    tx.executeSql(createStatement, [], showRecords, onError); });
+
+}
+
+function insertRecord(){ // Get value from Input and insert record . Function Call when Save/Submit Button Click..
+  var idTemp = $('#id').val();
+  var nombreTemp = $('input:text[id=inputName]').val();
+  var nitTemp = $('input:text[id=inputVat]').val();
+  var dirTemp = $('input:text[id=inputStreet]').val();
+  var telTemp = $('input:text[id=inputPhone]').val();
+  console.log($('input:text[id=inputId]').val());
+  if($.trim(idTemp) == "" && $.trim(nombreTemp) !="" && $.trim(nitTemp)!="" ){
+    db.transaction(function (tx) {
+      tx.executeSql(insertStatement,
+        [ $.trim(nombreTemp), $.trim(nitTemp), $.trim(dirTemp), $.trim(telTemp)], loadAndReset, onError);
+      });
+    }
+    else if($.trim(idTemp) != ""  && $.trim(nombreTemp) !="" && $.trim(nitTemp) !="" ){
+      updateRecord();
+    }
+//tx.executeSql(SQL Query Statement,[ Parameters ] , Sucess Result Handler Function, Error Result Handler Function );
+}
+function deleteRecord(id){ // Get id of record . Function Call when Delete Button Click..
+    var iddelete = id.toString();
+    db.transaction(function (tx) {
+      tx.executeSql(deleteStatement,
+        [id], showRecords, onError); alert("Delete Sucessfully"); });
+    resetForm();
+}
+
+function updateRecord(){ // Get id of record . Function Call when Delete Button Click..
+    var nombreUpdate = $('input:text[id=inputName]').val().toString();
+    var nitUpdate = $('input:text[id=inputVat]').val().toString();
+    var dirUpdate = $('input:text[id=inputStreet]').val().toString();
+    var phoneUpdate = $('input:text[id=inputPhone]').val().toString();
+    var useridUpdate = $("#id").val();
+    db.transaction(function (tx) {
+      tx.executeSql(updateStatement,
+        [nombreUpdate, nitUpdate,dirUpdate,phoneUpdate, Number(useridUpdate)],
+        loadAndReset, onError); });
+}
+
+function dropTable(){ // Function Call when Drop Button Click.. Talbe will be dropped from database.
+
+    db.transaction(function (tx) {
+      tx.executeSql(dropStatement, [], showRecords, onError); });
+
+    resetForm();
+    initDatabase();
+
+}
+
+function loadRecord(i){ // Function for display records which are retrived from database.
+    var item = dataset.item(i);
+    $("#inputName").val((item['Nombre']).toString());
+    $("#inputVat").val((item['Nit']).toString());
+    $("#inputStreet").val((item['Direccion']).toString());
+    $("#inputPhone").val((item['Telefono']).toString());
+    $("#id").val((item['id']).toString());
+}
+
+function resetForm(){ // Function for reset form input values.
+
+    $("#inputName").val("");
+    $("#inputVat").val("");
+    $("#inputStreet").val("");
+    $("#inputPhone").val("");
+    $("#id").val("");
+}
+
+function loadAndReset(){ //Function for Load and Reset...
+    resetForm();
+    showRecords()
+}
+
+function onError(tx, error){ // Function for Hendeling Error...
+    alert(error.message);
+}
+
+function showRecords(){ // Function For Retrive data from Database Display records as list
+  $("#listado_contactos").html('')
+  db.transaction(function (tx) {
+    tx.executeSql(selectAllStatement, [], function (tx, result) {
+      dataset = result.rows;
+      for (var i = 0, item = null; i < dataset.length; i++) {
+        item = dataset.item(i);
+        var linkeditdelete = '<tr>'+
+        '<td>' + '<a href="#" onclick="fillInvoice('+ i +')">' + item['Nombre'] +'</a>'+ '</td>' +
+        '<td>' + item['Nit']    + '</td>' +
+        '<td>' + item['Telefono'] + '</td>' +
+        '<td>' + '<a href="#" onclick="loadRecord(' + i + ');">Editar</a>' + '</td>'+
+        '<td>' + '<a href="#" onclick="deleteRecord(' + item['id'] + ');">eliminar</a>'+'</td>'+
+        '</tr>';
+                $("#listado_contactos").append(linkeditdelete);
+            }
+        });
+    });
+}
+function sortRecords(value){ // Function For Retrive data from Database Display records as list
+  $("#listado_contactos").html('')
+  db.transaction(function (tx) {
+    tx.executeSql("select * from Contacts order by "+value+ " desc;" , [], function (tx, result) {
+      dataset = result.rows;
+
+      for (var i = 0, item = null; i < dataset.length; i++) {
+        item = dataset.item(i);
+        var linkeditdelete = '<tr>'+
+        '<td>' + '<a href="#" onclick="fillInvoice('+ i +')">' + item['Nombre'] +'</a>'+ '</td>' +
+        '<td>' + item['Nit']    + '</td>' +
+        '<td>' + item['Telefono'] + '</td>' +
+        '<td>' + '<a href="#" onclick="loadRecord(' + i + ');">Editar</a>' + '</td>'+
+        '<td>' + '<a href="#" onclick="deleteRecord(' + item['id'] + ');">eliminar</a>'+'</td>'+
+        '</tr>';
+                $("#listado_contactos").append(linkeditdelete);
+            }
+        });
+    });
+}
+function findRecords(){ // Function For Retrive data from Database Display records as list
+  var usernametemp = $('input:text[id=findNit]').val();
+  var filtro = $('button:button[name=filtro]').val();
+  console.log(filtro);
+
+  $("#listado_contactos").html('')
+  db.transaction(function (tx) {
+    tx.executeSql("select * from Contacts where Nit like('%"+ usernametemp +"%') order by id desc;", [], function (tx, result) {
+      dataset = result.rows;
+      for (var i = 0, item = null; i < dataset.length; i++) {
+        item = dataset.item(i);
+        var linkeditdelete = '<tr>'+
+        '<td>' + '<a href="#" onclick="fillInvoice('+ i +')">' + item['Nombre'] +'</a>'+ '</td>' +
+        '<td>' + item['Nit']    + '</td>' +
+        '<td>' + item['Telefono'] + '</td>' +
+        '<td>' + '<a href="#" onclick="loadRecord(' + i + ');">Editar</a>' + '</td>'+
+        '<td>' + '<a href="#" onclick="deleteRecord(' + item['id'] + ');">eliminar</a>'+'</td>'+
+        '</tr>';
+                $("#listado_contactos").append(linkeditdelete);
+            }
+        });
+    });
+}
+
+function fillInvoice(i){
+  $("#datos")
+  var item = dataset.item(i);
+  console.log(i, item['Nombre']);
+  document.getElementById("invoiceName").innerHTML = item['Nombre'];
+  document.getElementById("invoiceVat").innerHTML = item['Nit'];
+  document.getElementById("invioceStreet").innerHTML = item['Direccion'];
+  document.getElementById("invoicePhone").innerHTML = item['Telefono'];
+
+}
+$(document).ready(function (){
+;
+
+    //$("body").fadeIn(2000);
+    initDatabase();
+    $("#submitButton").click(insertRecord);  // Register Event Listener when button click.
+    $("#btnUpdate").click(updateRecord);
+    $("#btnReset").click(resetForm);
+    $("#btnDrop").click(dropTable);
+    $("#btnSort").click(sortRecords);
+    $("#btnFind").click(findRecords);
+
+});
---
+++ src/js/factura.js
@@ -0,0 +1,244 @@
+function fecha(){
+      let now = new Date();
+      let dia = now.getDate();
+      let mes = now.getMonth();
+      let anio = now.getFullYear();
+      document.getElementById("day").innerHTML=dia;
+      document.getElementById("month").innerHTML=mes;
+      document.getElementById("year").innerHTML=anio;
+      //console.log(fecha);
+    }
+
+
+var num2words = (function() {
+    function Unidades(num){
+        switch(num)
+        {
+            //case 0: return 'CERO';
+            case 1: return 'UN';
+            case 2: return 'DOS';
+            case 3: return 'TRES';
+            case 4: return 'CUATRO';
+            case 5: return 'CINCO';
+            case 6: return 'SEIS';
+            case 7: return 'SIETE';
+            case 8: return 'OCHO';
+            case 9: return 'NUEVE';
+        }
+
+        return '';
+    }//Unidades()
+
+    function Decenas(num){
+
+        let decena = Math.floor(num/10);
+        let unidad = num - (decena * 10);
+
+        switch(decena)
+        {
+            case 1:
+                switch(unidad)
+                {
+                    case 0: return 'DIEZ';
+                    case 1: return 'ONCE';
+                    case 2: return 'DOCE';
+                    case 3: return 'TRECE';
+                    case 4: return 'CATORCE';
+                    case 5: return 'QUINCE';
+                    default: return 'DIECI' + Unidades(unidad);
+                }
+            case 2:
+                switch(unidad)
+                {
+                    case 0: return 'VEINTE';
+                    default: return 'VEINTI' + Unidades(unidad);
+                }
+            case 3: return DecenasY('TREINTA', unidad);
+            case 4: return DecenasY('CUARENTA', unidad);
+            case 5: return DecenasY('CINCUENTA', unidad);
+            case 6: return DecenasY('SESENTA', unidad);
+            case 7: return DecenasY('SETENTA', unidad);
+            case 8: return DecenasY('OCHENTA', unidad);
+            case 9: return DecenasY('NOVENTA', unidad);
+            case 0: return Unidades(unidad);
+        }
+    }//Unidades()
+
+    function DecenasY(strSin, numUnidades) {
+        if (numUnidades > 0)
+            return strSin + ' Y ' + Unidades(numUnidades)
+
+        return strSin;
+    }//DecenasY()
+
+    function Centenas(num) {
+        let centenas = Math.floor(num / 100);
+        let decenas = num - (centenas * 100);
+
+        switch(centenas)
+        {
+            case 1:
+                if (decenas > 0)
+                    return 'CIENTO ' + Decenas(decenas);
+                return 'CIEN';
+            case 2: return 'DOSCIENTOS ' + Decenas(decenas);
+            case 3: return 'TRESCIENTOS ' + Decenas(decenas);
+            case 4: return 'CUATROCIENTOS ' + Decenas(decenas);
+            case 5: return 'QUINIENTOS ' + Decenas(decenas);
+            case 6: return 'SEISCIENTOS ' + Decenas(decenas);
+            case 7: return 'SETECIENTOS ' + Decenas(decenas);
+            case 8: return 'OCHOCIENTOS ' + Decenas(decenas);
+            case 9: return 'NOVECIENTOS ' + Decenas(decenas);
+        }
+
+        return Decenas(decenas);
+    }//Centenas()
+
+    function Seccion(num, divisor, strSingular, strPlural) {
+        let cientos = Math.floor(num / divisor)
+        let resto = num - (cientos * divisor)
+
+        let letras = '';
+
+        if (cientos > 0)
+            if (cientos > 1)
+                letras = Centenas(cientos) + ' ' + strPlural;
+            else
+                letras = strSingular;
+
+        if (resto > 0)
+            letras += '';
+
+        return letras;
+    }//Seccion()
+
+    function Miles(num) {
+        let divisor = 1000;
+        let cientos = Math.floor(num / divisor)
+        let resto = num - (cientos * divisor)
+
+        let strMiles = Seccion(num, divisor, 'UN MIL', 'MIL');
+        let strCentenas = Centenas(resto);
+
+        if(strMiles == '')
+            return strCentenas;
+
+        return strMiles + ' ' + strCentenas;
+    }//Miles()
+
+    function Millones(num) {
+        let divisor = 1000000;
+        let cientos = Math.floor(num / divisor)
+        let resto = num - (cientos * divisor)
+
+        let strMillones = Seccion(num, divisor, 'UN MILLON DE', 'MILLONES DE');
+        let strMiles = Miles(resto);
+
+        if(strMillones == '')
+            return strMiles;
+
+        return strMillones + ' ' + strMiles;
+    }//Millones()
+
+    return function NumeroALetras(num, currency) {
+        currency = currency || {};
+        let data = {
+            numero: num,
+            enteros: Math.floor(num),
+            centavos: (((Math.round(num * 100)) - (Math.floor(num) * 100))),
+            letrasCentavos: 'EXACTOS',
+            letrasMonedaPlural: currency.plural || 'QUETZALES',//'PESOS', 'Dólares', 'Bolívares', 'etcs'
+            letrasMonedaSingular: currency.singular || 'QUETZAL', //'PESO', 'Dólar', 'Bolivar', 'etc'
+            letrasMonedaCentavoPlural: currency.centPlural || 'CENTAVOS',
+            letrasMonedaCentavoSingular: currency.centSingular || 'CENTAVO'
+        };
+
+        if (data.centavos > 0) {
+            data.letrasCentavos = 'CON ' + (function () {
+                    if (data.centavos == 1)
+                        return Millones(data.centavos) + ' ' + data.letrasMonedaCentavoSingular;
+                    else
+                        return Millones(data.centavos) + ' ' + data.letrasMonedaCentavoPlural;
+                })();
+        };
+
+        if(data.enteros == 0)
+            return 'CERO ' + data.letrasMonedaPlural + ' ' + data.letrasCentavos;
+        if (data.enteros == 1)
+            return Millones(data.enteros) + ' ' + data.letrasMonedaSingular + ' ' + data.letrasCentavos;
+        else
+            return Millones(data.enteros) + ' ' + data.letrasMonedaPlural + ' ' + data.letrasCentavos;
+    };
+
+
+}
+)();
+
+
+
+function sumar () {
+      var subtotal=0.00;
+      let total=0.00
+      var precio= document.getElementsByName("valor");
+      for (var index=0; index < 9; index++ ){
+          var celda = 0.00;
+          celda = parseFloat(precio[index].innerHTML);
+          celda =celda.toFixed(2);
+          if ( !isNaN(celda )){
+            precio[index].innerHTML = celda
+          }
+
+        if ( precio[index].innerHTML !=0 && !isNaN(precio[index].innerHTML)) {
+          subtotal += parseFloat(precio[index].innerHTML);
+         //console.log("2do bloq if ",subtotal);
+        }
+        else{
+         //console.log(celda);
+          //break;
+        }
+      }
+    total = (parseFloat(subtotal)).toFixed(2);
+    console.log(total);
+
+   //console.log(total);
+    document.getElementById("total").innerHTML=new Intl.NumberFormat(
+      "en-US",{ minimumFractionDigits: 2 }).format(total);
+
+   //console.log(num2words(total));
+
+    console.log(new Intl.NumberFormat("en-US",{ minimumFractionDigits: 2 }).format(total));
+
+    document.getElementById("totalletras").innerHTML =num2words(total);
+    }
+
+function borrar(){
+
+    var td= document.querySelectorAll("td");
+   //console.log("B",td[0].innerHTML);
+    for (var index=3; index < 37; index++ ){
+      if (index==7) index = 10; //salo par limpiar
+        document.querySelectorAll("td")[index].innerHTML = "";
+
+    }
+
+  //  var db = openDatabase('db.sqlite', '1.0', 'Test DB', 2 * 1024 * 1024);
+}
+
+function imprimir(){
+    sumar();
+    print();
+
+}
+
+
+
+
+    // function sumar() {
+    //   var precio = document.querySelectorAll(".val").values();
+    //  //console.log(precio)
+    //   // precio.forEach((item, i) => {
+    //   //var cantidad=parseFloat(columnas[1].textContent);
+    //   // });
+    //
+    // };
+    //console.log(parseFloat(precio[0].innerHTML));
